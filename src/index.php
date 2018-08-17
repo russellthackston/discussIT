7fb9d6e631da9b1a4c520ff6c3400e45f21014da
Russell Thackston <rthackston@georgiasouthern.edu>
2018-08-17 20:58:02 +0000

    Removed unused declarations

--- src/index.php
+++ src/index.php
@@ -1,14 +1,11 @@
 <?php
-	
+
 // Import the application classes
 require_once('include/classes.php');
 
 // Create an instance of the Application class
 $app = new Application();
 $app->setup();
-
-// Declare an empty array of error messages
-$errors = array();
 
 ?>
 
@@ -22,11 +19,11 @@ $errors = array();
 	<main id="wrapper">
 		<h2>IT 3234 Class Discussions</h2>
 		<p>
-			This private discussion site is for students 
-			enrolled in Dr. Thackston's IT courses. 
-			Students currently registered in one of my courses may 
-			<a href="register.php">create an account</a> 
-			or proceed directly to the 
+			This private discussion site is for students
+			enrolled in Dr. Thackston's IT courses.
+			Students currently registered in one of my courses may
+			<a href="register.php">create an account</a>
+			or proceed directly to the
 			<a href="login.php">login page</a>.
 		</p>
 	</main>
