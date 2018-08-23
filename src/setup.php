0a18f0cec2290929cb0f119e990874ddd9bc688f
Russell Thackston <rthackston@georgiasouthern.edu>
2018-08-22 20:39:23 -0400

    Added database setup form elements

--- src/setup.php
+++ src/setup.php
@@ -2,21 +2,31 @@
 <html lang="en">
 <?php include 'include/head.php'; ?>
 <body>
-	<!--?php include 'include/header.php'; ?-->
 	<div id="barba-wrapper">
 	<div class="barba-container">
 	<main id="wrapper">
 		<h2>IT 3234 Class Discussions</h2>
 		<p>
 			First time setup required.
+			Please provide the following information and click the Setup button.
 		</p>
+		<form action="setup.php" method="post" name="setupdbform">
+			<label for="servername">Database server name:</label>
+			<input type="text" name="servername" id="servername">
+			<br>
+			<label for="serverusername">Database username:</label>
+			<input type="text" name="serverusername" id="serverusername">
+			<br>
+			<label for="serverdbname">Database name:</label>
+			<input type="text" name="serverdbname" id="serverdbname">
+			<br>
+			<label for="serverpassword">Database password:</label>
+			<input type="password" name="serverpassword" id="serverpassword">
+			<br>
+			<input type="submit" name="setupdb" id="setupdb">Setup!</input>
+		</form>
 	</main>
 	</div>
 	</div>
-	<!--?php include 'include/footer.php'; ?-->
-	<!--script src="js/jquery-3.3.1.min.js"></script>
-	<script src="js/site.js"></script>
-	<script src="js/barba.js"></script>
-	<script src="js/mybarba.js"></script-->
 </body>
 </html>
