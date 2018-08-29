045ac74a32674969a0110ce3e4528ed04b74e0f0
Russell Thackston <rthackston@georgiasouthern.edu>
2018-08-28 20:26:46 -0400

    Cleaned up student name display in delete confirmation prompt.

--- src/js/site.js
+++ src/js/site.js
@@ -411,13 +411,13 @@ function addStudent() {
 function deleteStudent(elem) {
 	var href = replacePage(window.location.href, "deletestudent.php");
 	var studentid = elem.getAttribute('data-studentid');
-	var studentname = document.getElementById('newstudentname');
+	var studentname = elem.getElementById('student-name-' + studentid);
 	var row = document.getElementById('student-row-' + studentid);
 	var data = {
 		"studentid" : studentid
 	};
 
-	var r = confirm("Are you sure you want to delete '" + studentname.value + "'");
+	var r = confirm("Are you sure you want to delete '" + studentname.innerHTML + "'");
 	if (r != true) {
 	    return;
 	}
