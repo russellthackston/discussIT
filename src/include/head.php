abb3c6ff959a226e61dea6dfde1f51f87758f923
Russell Thackston <rthackston@georgiasouthern.edu>
2018-10-10 20:52:22 -0400

    Added versioning to JS and CSS includes

--- src/include/head.php
+++ src/include/head.php
@@ -5,12 +5,6 @@
 	<meta name="author" content="Russell Thackston">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<link rel="stylesheet" href="css/style.css">
-		<?php
-			$folder = $_SERVER['PHP_SELF'];
-			if (strpos($folder, 'discussion-qa') !== false || strpos($folder, 'discussion-dev') !== false) {
-		?>
-			<link rel="stylesheet" href="css/style-qa.css">
-		<?php } ?>
+	<link rel="stylesheet" href="css/style.css?v=<?php echo $app->getVersion(); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
